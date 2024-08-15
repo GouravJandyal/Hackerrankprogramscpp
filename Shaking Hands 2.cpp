161a50e0e210875d0f1e413e2cde7a5764c40901
GouravJandyal <95241725+GouravJandyal@users.noreply.github.com>
2024-08-16 01:04:30 +0530

    Create Shaking Hands 2.cpp

---
+++ Shaking Hands 2.cpp
@@ -0,0 +1,31 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    int N;
+     cin>>N;
+     int ans = (N*(N-1))/2;
+     cout<<ans;
+    return 0;
+}
